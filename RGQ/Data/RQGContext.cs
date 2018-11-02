db80eeedf4df0568e14c06759fe5d487bdb3e654
Ben Ward <benjamin.e.ward@outlook.com>
2018-11-02 11:50:01 -0400

    Move 'reveal answer' in Random view

---
+++ RGQ/Data/RQGContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RQG.Models
+{
+    public class RQGContext : DbContext
+    {
+        public RQGContext (DbContextOptions<RQGContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RGQ.Models.Question> Question { get; set; }
+    }
+}
